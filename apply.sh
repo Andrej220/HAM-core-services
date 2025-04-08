b0c323e4ba887e86cc75c13abf60248ca9177e84
Andrey Zargarov <andrey.zargarov@gmail.com>
2025-04-08 14:14:04 +0200

    feature(install) - added apply.sh to install all applications at once

---
+++ apply.sh
@@ -0,0 +1,4 @@
+kubectl apply -f applications/storage.yaml -n argocd
+kubectl apply -f applications/postgresql.yaml -n argocd
+kubectl apply -f applications/mongodb.yaml -n argocd
+kubectl apply -f applications/db-init.yaml -n argocd
