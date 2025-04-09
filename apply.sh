cd7815dae50bc80566c4e750a0ff19b554962506
Andrey Zargarov <andrey.zargarov@gmail.com>
2025-04-09 19:19:52 +0200

    refactor(mongodb) - separates update password into a task

--- apply.sh
+++ apply.sh
@@ -1,4 +1,5 @@
 kubectl apply -f applications/storage.yaml -n argocd
-kubectl apply -f applications/postgresql.yaml -n argocd
 kubectl apply -f applications/mongodb.yaml -n argocd
+kubectl apply -f applications/postgresql.yaml -n argocd
 kubectl apply -f applications/db-init.yaml -n argocd
+kubectl apply -f applications/mongodb-update-password.yaml -n argocd
