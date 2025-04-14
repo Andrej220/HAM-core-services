1528a8395587c51e0f1b263bd9dddf9cfb9a7e20
Andrey Zargarov <andrey.zargarov@gmail.com>
2025-04-14 21:44:23 +0200

    refactor(uninstall) - delete all including PVs and PVCs

--- del.sh
+++ del.sh
@@ -4,4 +4,7 @@ kubectl delete -f applications/mongodb.yaml -n argocd
 kubectl delete -f applications/db-init.yaml -n argocd
 kubectl delete secret --all -n database-test
 kubectl delete all --all  -n database-test
+kubectl delete pvc --all -n database-test
+kubectl delete pv pv-raid10-postgresql
+kubectl delete pv pv-raid10-mongodb
 kubectl get all  -n database-test
