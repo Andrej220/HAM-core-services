87dd82229b682c3b260e9e1c1564e5faa26ac254
Andrey Zargarov <andrey.zargarov@gmail.com>
2025-04-17 23:19:31 +0200

    fix(mongodb install) - removes namespace from kubectl apply for mongodb as secret is embeded ingo mongodb manifest

--- del.sh
+++ del.sh
@@ -1,6 +1,6 @@
 kubectl delete -f applications/storage.yaml -n argocd
 kubectl delete -f applications/postgresql.yaml -n argocd
-kubectl delete -f applications/mongodb.yaml -n argocd
+kubectl delete -f applications/mongodb.yaml 
 kubectl delete -f applications/db-init.yaml -n argocd
 kubectl delete secret --all -n database-test
 kubectl delete all --all  -n database-test
