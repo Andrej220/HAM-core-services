5fc384099ce37c3a0ff978a4359f5830e9ab6759
Andrey Zargarov <andrey.zargarov@gmail.com>
2025-04-19 23:24:02 +0200

    refactor(postgresql) - moves password to postgresql secret, makes manifest cleaner

--- apply.sh
+++ apply.sh
@@ -1,4 +1,4 @@
 kubectl apply -f applications/storage.yaml -n argocd
 kubectl apply -f applications/mongodb.yaml 
-kubectl apply -f applications/postgresql.yaml -n argocd
+kubectl apply -f applications/postgresql.yaml 
 kubectl apply -f applications/db-init.yaml -n argocd
