a1b3360a6bb263bd6d1135f2808692a15f9b1895
Andrey Zargarov <andrey.zargarov@gmail.com>
2025-04-21 16:24:49 +0200

    [del.sh] - fixes namespace to delete secrets, pvc ... from correct namespace - database

--- del.sh
+++ del.sh
@@ -2,9 +2,9 @@ kubectl delete -f applications/storage.yaml -n argocd
 kubectl delete -f applications/postgresql.yaml 
 kubectl delete -f applications/mongodb.yaml 
 kubectl delete -f applications/db-init.yaml -n argocd
-kubectl delete secret --all -n database-test
-kubectl delete all --all  -n database-test
-kubectl delete pvc --all -n database-test
+kubectl delete secret --all -n database
+kubectl delete all --all  -n database
+kubectl delete pvc --all -n database
 kubectl delete pv pv-raid10-postgresql
 kubectl delete pv pv-raid10-mongodb
-kubectl get all  -n database-test
+kubectl get all  -n database
